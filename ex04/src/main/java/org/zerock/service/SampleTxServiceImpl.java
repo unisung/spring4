54047e3509b47d64f1f281213b3f9a89498cabff
unisung <vctor@naver.com>
2020-09-18 12:41:12 +0900

    d

--- ex04/src/main/java/org/zerock/service/SampleTxServiceImpl.java
+++ ex04/src/main/java/org/zerock/service/SampleTxServiceImpl.java
@@ -2,6 +2,7 @@ package org.zerock.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mapper.Sample1Mapper;
 import org.zerock.mapper.Sample2Mapper;
 
@@ -18,7 +19,7 @@ public class SampleTxServiceImpl implements SampleTxService {
 	@Setter(onMethod_=@Autowired)
 	private Sample2Mapper mapper2;
 	
-	
+	@Transactional
 	@Override
 	public void addData(String value) {
 	   
